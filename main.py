4d1aa7110e1c9e874e033a3651383f98ca76b7ad
loggylogzorce <loggylogzorce@yandex.ru>
2025-11-07 13:58:06 +0300

    fix2 main.py

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
 from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
-
-
-@app.route('/echo', methods=['POST'])
-def echo():
-    data = request.get_json()
-    return jsonify({
-        "status": "received",
-        "you_sent": data,
-        "length": len(str(data)) if data else 0
-    })
-
-
-from flask import Flask, request, jsonify
 import psycopg2
 import os
 from urllib.parse import urlparse
@@ -78,6 +58,21 @@ def get_messages():
 
     messages = [{"id": r[0], "text": r[1], "time": r[2].isoformat()} for r in rows]
     return jsonify(messages)
+
+
+@app.route('/')
+def hello():
+    return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json()
+    return jsonify({
+        "status": "received",
+        "you_sent": data,
+        "length": len(str(data)) if data else 0
+    })
 
 
 if __name__ == '__main__':
